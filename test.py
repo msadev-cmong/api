e290fb655cd544b1ee2e5b76e1c6748ea9cbd4d9
sook22002 <seonghun2407@gmail.com>
2022-04-22 01:24:42 +0900

    api get

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import pandas as pd
-data = pd.read_csv('sido.csv')
-print(data)
+data = pd.read_csv('전국_이송환자수.csv')
+sido = data.where((pd.notnull(data)), None)
+for i in range(len(sido)):
+    print(tuple(sido.values[i]))
